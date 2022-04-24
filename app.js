87ef2407b0385bdec38bdba8842de4a1e618d001
Bucky Collins <bucky.collins.67@gmail.com>
2022-04-24 14:30:03 -0700

    Solution Aquired

--- app.js
+++ app.js
@@ -6,20 +6,25 @@
 // 0 => [0]
 
 function digitize(n) {
-    //code here
+    let numToArray = Array.from(String(n), Number)
+    return numToArray.reverse()
   }
-
 
 //PREP Method
 
 // P - Parameters
+// Random non-negative number.  Any number of digits.
 
 
 // R - Return
+// Return an array.  Input reversed, with each number in it's own index.
 
 
 // E - Examples
-
+// 348597 => [7,9,5,8,4,3]
+// 0 => [0]
 
 // P - Pseudocode
-
+// 1.) Convert the number to an array
+// 2.) Reverse the array
+// 3.) Return that array
